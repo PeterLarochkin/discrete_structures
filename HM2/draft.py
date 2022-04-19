a1bdafcb9e3ffff4124cb296bf2bc9a402e8d85f
Peter <plarochkin@gmail.com>
2022-04-20 00:12:16 +0300

    add 3 requirements

--- HM2/draft.py
+++ HM2/draft.py
@@ -1,3 +1,4 @@
+from typing import final
 from PyMiniSolvers import minisolvers
 # S = minisolvers.MinisatSolver()
 # for i in range(4):
@@ -7,6 +8,94 @@ from PyMiniSolvers import minisolvers
 # S.solve()
 # получим решение
 # print(list(S.get_model()))
+
+
+variables = []
+# https://logic.pdmi.ras.ru/~arist/papers/sat09.pdf
+# basis is {Sheffer stroke}
+
+# N - quantity of element
+# n - input variables
+n = 0
+def requirement1(N):
+    temp = []
+    for i in range(n, n + N):
+        variables.append("t_{}_0_0".format(i))
+        variables.append("t_{}_0_1".format(i))
+        variables.append("t_{}_1_0".format(i))
+        variables.append("t_{}_1_1".format(i))
+
+        temp.append("t_{}_0_0".format(i))
+        temp.append("t_{}_0_1".format(i))
+        temp.append("t_{}_1_0".format(i))
+        temp.append("-t_{}_1_1".format(i))
+    return "Λ".join(temp)
+
+def requirement2(N):
+    temp = []
+    finalClause = []
+    vars = []
+    for i in range(n, n + N):
+        for j in range(0, n + N):
+            variables.append("c_{}_0_{}".format(i, j))
+            variables.append("c_{}_1_{}".format(i, j))
+
+            vars.append("c_{}_0_{}".format(i, j))
+            vars.append("c_{}_1_{}".format(i, j))
+
+            temp.append("{}_0_{}'c_{}_0_{}".format(i, j, i, j))
+            temp.append("{}_1_{}'c_{}_1_{}".format(i, j, i, j))
+    for i in range(n, n + N):
+        for k in range(0, 2):
+            # localVars = list(filter(lambda var: var[:3] == "{}_{}".format(i, k), temp))
+            # clause = "("+"V".join(localVars)+")"
+            # количество j переменной n+N
+            for num in range(2**(n+N)):
+                binNum =  "{}".format(bin(num))[2:]
+                binNum = (("0"*((n+N) - len(binNum)))+binNum)
+                if binNum.count("1") != 1:
+                    temp2 = []
+                    for j, item in enumerate(binNum):
+                        replaced = "-" if item == "1" else ""
+                        clause = replaced+"c_{}_{}_{}".format(i, k, j)
+                        temp2.append(clause)
+                    finalClause.append("("+ "V".join(temp2) + ")")
+    print(vars)
+    return "Λ".join(finalClause)
+
+def requirement3(N):
+    finalClause = []
+    for i in range(n, n + N):
+        variables.append("o_{}_{}".format(i, 0))
+    for num in range(2**(n+N)):
+        binNum =  "{}".format(bin(num))[2:]
+        binNum = (("0"*((n+N) - len(binNum)))+binNum)
+        if binNum.count("1") != 1:
+            temp2 = []
+            for i, item in enumerate(binNum):
+                replaced = "-" if item == "1" else ""
+                clause = replaced+"o_{}_{}".format(i, 0)
+                temp2.append(clause)
+            finalClause.append("("+ "V".join(temp2) + ")")
+    return "Λ".join(finalClause)
+
+
+
+exit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 isPow2 = lambda n: ((n&(n-1)==0) and n) and (True) or (False)
 
 vectorOfValue = "1110001011100010"
